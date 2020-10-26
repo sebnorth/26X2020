769b46a533c68f4877f6db6a689bf9d10ed9b165
sebnorth <sebnorth@gmail.com>
2020-10-26 15:33:47 +0100

    Dodaj pliki projektów.

---
+++ DodajDwieLiczbyWersja2/Obliczenia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodajDwieLiczbyWersja2
+{
+    class Obliczenia
+    {
+        public static void ObliczSume(int x_parametr, int y_parametr) {
+
+            Console.WriteLine($"Suma podanych wartości wynosi: {x_parametr + y_parametr}");
+
+        }
+    }
+}
---
+++ DodajDwieLiczbyWersja2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodajDwieLiczbyWersja2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            int.TryParse(Console.ReadLine(), out int x);
+
+            Console.WriteLine("Podaj drugą liczbę: ");
+            int.TryParse(Console.ReadLine(), out int y);
+
+            Obliczenia.ObliczSume(x, y);
+
+            Console.ReadKey();
+
+        }
+    }
+}
