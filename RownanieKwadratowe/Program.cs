ec06a1e0d457db1684a7f8fbebba7497940049fe
sebnorth <sebnorth@gmail.com>
2020-10-26 17:41:32 +0100

    Dodano temat wyjątki i zadanie Równanie kwadratowe.

---
+++ RownanieKwadratowe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RownanieKwadratowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Podaj a,b,c CAŁKOWITE
+            // 2. obliczamy deltę = b*b - 4*a*c 
+            // Math.Pow(b,2)
+            // delta < 0 
+            // delta = 0
+            // delta > 0 
+            // 3. trzeba znaleźć wzory na pierwiastki
+            // 
+
+            //double x = (6 / 7 > 0) ? (6 / 7) : (7 / 6);
+            //Console.WriteLine(x);
+
+            // Console.WriteLine((double)6 / 7);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Wyjatki/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyjatki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        naprawa:
+            try
+            {
+                
+                int x = 10;
+                
+                
+                int y = Convert.ToInt32(Console.ReadLine());
+                int wynik = x / y;
+                Console.WriteLine(wynik);
+            }
+
+            catch (DivideByZeroException uchwyt)
+            {
+                Console.WriteLine(uchwyt.Message);
+
+                // throw;
+            }
+
+            catch (Exception uchwyt)
+            {
+                Console.WriteLine(uchwyt.Message);
+                goto naprawa;
+            }
+
+        
+
+            //finally {
+                
+            //    Console.WriteLine("Program rusza dalej");
+            //    double dzielnik, iloraz;
+            //    try
+            //    {
+            //        dzielnik = Convert.ToDouble(Console.ReadLine());
+            //        if (dzielnik == 0)
+            //            throw new DivideByZeroException("Dzielnik nie może mieć wartość zero!!!");
+            //        iloraz = 10.2 / dzielnik; Console.WriteLine(iloraz);
+            //    }
+            //    catch (DivideByZeroException ex)
+            //    {
+            //        Console.WriteLine("Komunikat wyjatku: {0}", ex.Message);
+            //    }
+
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
